467882477881564e078842641ac189a6141663ba
Mathieu SABARTHES <matthieu-externe.sabarthes@enedis.fr>
2025-10-09 17:07:53 +0200

    chore: format enedis.tic.service.endpoint package

--- src/main/java/enedis/tic/service/endpoint/EventSender.java
+++ src/main/java/enedis/tic/service/endpoint/EventSender.java
@@ -10,13 +10,33 @@ package enedis.tic.service.endpoint;
 import enedis.lab.util.message.Event;
 import io.netty.channel.Channel;
 
-/** Event sender interface */
+/**
+ * Interface for sending event messages to a WebSocket channel.
+ *
+ * <p>This interface defines the contract for dispatching {@link Event} objects to a specified Netty
+ * {@link Channel}. Implementations are responsible for serializing and transmitting event data over
+ * the channel.
+ *
+ * <p>Key features include:
+ *
+ * <ul>
+ *   <li>Abstracts event delivery to WebSocket clients
+ *   <li>Supports custom event serialization and transmission strategies
+ * </ul>
+ *
+ * @author Enedis Smarties team
+ * @see Event
+ * @see Channel
+ */
 public interface EventSender {
   /**
-   * Send event
+   * Sends an event message to the specified WebSocket channel.
    *
-   * @param channel
-   * @param event
+   * <p>Implementations should serialize the event and transmit it to the client associated with the
+   * given channel.
+   *
+   * @param channel the Netty WebSocket channel to send the event to
+   * @param event the event message to be sent
    */
-  public void sendEvent(Channel channel, Event event);
+  void sendEvent(Channel channel, Event event);
 }
--- src/main/java/enedis/tic/service/endpoint/TIC2WebSocketEndPointException.java
+++ src/main/java/enedis/tic/service/endpoint/TIC2WebSocketEndPointException.java
@@ -8,19 +8,34 @@
 package enedis.tic.service.endpoint;
 
 /**
- * @author Antoine
+ * Exception class for TIC2WebSocket endpoint errors.
+ *
+ * <p>This exception encapsulates an error code from {@link TIC2WebSocketEndPointErrorCode},
+ * providing detailed context for endpoint operation failures. It supports multiple constructors for
+ * message and cause chaining.
+ *
+ * <p>Key features include:
+ *
+ * <ul>
+ *   <li>Associates an error code with each exception
+ *   <li>Supports message and cause chaining for diagnostics
+ * </ul>
+ *
+ * @author Enedis Smarties team
+ * @see TIC2WebSocketEndPointErrorCode
  */
-/** TIC2WebSocket end point Exception */
 public class TIC2WebSocketEndPointException extends Exception {
 
+  /** Serial version UID for serialization. */
   private static final long serialVersionUID = -2263755971102386572L;
 
-  private TIC2WebSocketEndPointErrorCode code;
+  /** Error code associated with this exception. */
+  private final TIC2WebSocketEndPointErrorCode code;
 
   /**
-   * Default constructor
+   * Constructs an exception with the specified error code.
    *
-   * @param code
+   * @param code the error code
    */
   public TIC2WebSocketEndPointException(TIC2WebSocketEndPointErrorCode code) {
     super();
@@ -28,11 +43,11 @@ public class TIC2WebSocketEndPointException extends Exception {
   }
 
   /**
-   * Constructor using message and cause
+   * Constructs an exception with the specified error code, message, and cause.
    *
-   * @param code
-   * @param message
-   * @param cause
+   * @param code the error code
+   * @param message the detail message
+   * @param cause the cause of the exception
    */
   public TIC2WebSocketEndPointException(
       TIC2WebSocketEndPointErrorCode code, String message, Throwable cause) {
@@ -41,10 +56,10 @@ public class TIC2WebSocketEndPointException extends Exception {
   }
 
   /**
-   * Constructor using message
+   * Constructs an exception with the specified error code and message.
    *
-   * @param code
-   * @param message
+   * @param code the error code
+   * @param message the detail message
    */
   public TIC2WebSocketEndPointException(TIC2WebSocketEndPointErrorCode code, String message) {
     super(message);
@@ -52,10 +67,10 @@ public class TIC2WebSocketEndPointException extends Exception {
   }
 
   /**
-   * Constructor using cause
+   * Constructs an exception with the specified error code and cause.
    *
-   * @param code
-   * @param cause
+   * @param code the error code
+   * @param cause the cause of the exception
    */
   public TIC2WebSocketEndPointException(TIC2WebSocketEndPointErrorCode code, Throwable cause) {
     super(cause);
@@ -63,9 +78,9 @@ public class TIC2WebSocketEndPointException extends Exception {
   }
 
   /**
-   * Get error code
+   * Returns the error code associated with this exception.
    *
-   * @return error code
+   * @return the endpoint error code
    */
   public TIC2WebSocketEndPointErrorCode getCode() {
     return this.code;
