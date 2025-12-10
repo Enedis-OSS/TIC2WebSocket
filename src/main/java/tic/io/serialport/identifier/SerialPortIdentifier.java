6718000ef550e8c51c361913d0c2aef1b95636e8
Matthieu SABARTHES <matthieu-externe.sabarthes@enedis.fr>
2025-12-10 14:53:59 +0100

    fix: REUSE compliance

--- src/main/java/tic/io/serialport/identifier/SerialPortIdentifier.java
+++ src/main/java/tic/io/serialport/identifier/SerialPortIdentifier.java
@@ -1,3 +1,10 @@
+// Copyright (C) 2025 Enedis Smarties team <dt-dsi-nexus-lab-smarties@enedis.fr>
+//
+// SPDX-FileContributor: Jehan BOUSCH
+// SPDX-FileContributor: Mathieu SABARTHES
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package tic.io.serialport.identifier;
 
 /** Class representing a serial port identifier, which can be either by name or by ID. */
