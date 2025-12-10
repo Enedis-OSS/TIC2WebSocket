53dba0943b8ef488bea8c2feeac7e11bcaca6c9a
Matthieu SABARTHES <matthieu-externe.sabarthes@enedis.fr>
2025-12-10 11:25:42 +0100

    fix: similar filepath for checksum unit tests

--- src/test/java/tic/frame/checksum/TICChecksumTest.java
+++ src/test/java/tic/frame/checksum/TICChecksumTest.java
@@ -5,12 +5,12 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-package tic.frame;
+package tic.frame.checksum;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import tic.frame.checksum.TICChecksum;
+import tic.frame.TICMode;
 
 public class TICChecksumTest {
   @Test
