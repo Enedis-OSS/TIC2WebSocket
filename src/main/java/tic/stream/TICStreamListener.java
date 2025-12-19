32a3b292a172dbfc4762ca78de39f88d29fd7088
Mathieu SABARTHES <matthieu-externe.sabarthes@enedis.fr>
2025-12-19 15:35:05 +0100

    fix: add REUSE compliance headers

--- src/main/java/tic/stream/TICStreamListener.java
+++ src/main/java/tic/stream/TICStreamListener.java
@@ -1,3 +1,10 @@
+// Copyright (C) 2025 Enedis Smarties team <dt-dsi-nexus-lab-smarties@enedis.fr>
+//
+// SPDX-FileContributor: Jehan BOUSCH
+// SPDX-FileContributor: Mathieu SABARTHES
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package tic.stream;
 
 import tic.frame.TICFrame;
