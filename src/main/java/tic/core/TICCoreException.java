08b3a6d470a089359003e2915d18119061f443c8
Matthieu SABARTHES <matthieu-externe.sabarthes@enedis.fr>
2026-01-06 15:33:49 +0100

    refactor: duplicate enedis.tic.core into tic.core

---
+++ src/main/java/tic/core/TICCoreException.java
@@ -0,0 +1,26 @@
+// Copyright (C) 2025 Enedis Smarties team <dt-dsi-nexus-lab-smarties@enedis.fr>
+//
+// SPDX-FileContributor: Jehan BOUSCH
+// SPDX-FileContributor: Mathieu SABARTHES
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package tic.core;
+
+import enedis.lab.types.ExceptionBase;
+
+/**
+ * Exception class for representing core errors and failures.
+ *
+ * <p>This class provides mechanisms for constructing exceptions with error codes and messages,
+ * supporting structured error handling and reporting.
+ *
+ * @author Enedis Smarties team
+ */
+public class TICCoreException extends ExceptionBase {
+  private static final long serialVersionUID = -3285641164559292710L;
+
+  public TICCoreException(int code, String info) {
+    super(code, info);
+  }
+}
